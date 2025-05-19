dbc3960a8ad0ec203be6d5a68d4624f28b0d5f32
Bezet1 <joszko90@gmail.com>
2025-05-19 22:08:48 +0200

    Change config

--- src/config.py
+++ src/config.py
@@ -1,13 +1,9 @@
 import os
 
-# Ścieżka do katalogu z obrazami treningowymi
 DATA_DIR = os.path.join("screen_collector", "screens")
 
-# Ścieżka do zapisanego (wytrenowanego) modelu
 MODEL_PATH = os.path.join("trained_models", "best_model.h5")
 
-# Nazwy klas odpowiadające kierunkom ruchu w grze
 CLASS_NAMES = ['DOWN', 'LEFT', 'RIGHT', 'UP']
 
-# Przykładowy obraz do przewidywania (do testów)
-SAMPLE_IMAGE_PATH = os.path.join("screen_collector", "screens", "DOWN", "example2.png")
+SAMPLE_IMAGE_PATH = os.path.join("screen_collector", "screens", "DOWN", "test.png")
